382b946e3c4d02d78110e23c97e385e28cb92454
Hangris <arislsm513@gmail.com>
2025-11-04 04:41:51 +0700

    First commit - Secure API Project

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+
+// Koneksi ke MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch(err => console.log("❌ MongoDB Error:", err));
+
+// Hubungkan route dari file routes/auth.js
+const authRoutes = require("./routes/auth");
+app.use("/api", authRoutes); // prefix: /api
+app.use(express.static("public"));
+
+
+app.listen(3000, () => console.log("🚀 Server running on port 3000"));
